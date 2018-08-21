b408ddd54d407b31b59059934c7818127786721b
Darja Ballarinova <darja.ballarinova@gmail.com>
2018-08-21 20:57:54 +0200

    Consolidate states and functions under Mainpage

--- src/List-of-playgrounds.js
+++ src/List-of-playgrounds.js
@@ -1,27 +1,8 @@
 import React, { Component } from 'react'
-import ReactPropTypes from 'prop-types'
-import escapeRegExp from 'escape-string-regexp'
-import sortBy from 'sort-by'
 import { Link } from 'react-router-dom'
-import Map from './Map';
 
-let searchedPlaygrounds
 
 class List extends Component {
-  static ReactPropTypes = {
-    playgroundsDetail: ReactPropTypes.array.isRequired
-  }
-
-  state = {
-    search: ''
-  }
-  
-  searchAsk = (ask) => {
-    this.setState({
-      search: ask.trim()
-    })
-  }
-
 
   render () {
 
@@ -29,40 +10,29 @@ class List extends Component {
         const { search } = this.state
         const { playgrounds } = this.props */
     
-      
-
-    if (this.state.search) {
-      const result = new RegExp(escapeRegExp(this.state.search), 'i')
-      searchedPlaygrounds = this.props.playgrounds.filter((playground) => result.test(playground.city))
-      searchedPlaygrounds.sort(sortBy('address'))
-    } else {
-      searchedPlaygrounds = this.props.playgrounds
-      searchedPlaygrounds.sort(sortBy('city'))
-    }
-
-    
+    let searchedPlaygrounds = this.props.searchedPlaygroundsFromList
 
 		return (
-      <div id="mainContent">
-            <div id= "list">
-              <Link className="add-new-playground" to='/add'>
-                <div className='plus-sign'></div>
-                <div className='add-playground'>Add new playground</div>
+      
+            <div id= "sidebar">
+              <Link id="add-new-playground" to='/add'>
+                <div id="plus-sign"></div>
+                <div id="add-playground">Add new playground</div>
               </Link>
-              <div className='search-field'>
-                <div className='search-sign'></div>                
+              <div id="search">
+                <div id="search-sign"></div>                
                 <input 
                   type="text" 
                   placeholder="Search place by City" 
-                  id="searchField"
-                  value={this.state.search}
+                  id="search-field"
+                  value={this.props.search}
                   onChange={(event) => 
                   this.searchAsk(event.target.value)}
                 />
               </div>
                   {/*{JSON.stringify(this.state.search)}*/}
 
-              <ol className="ListOfPG">
+              <ol id="list">
                 {searchedPlaygrounds.map((playground) => (
                   <li key={playground.lng}>
                     {playground.city}, {playground.address}
@@ -70,11 +40,6 @@ class List extends Component {
                   ))}
               </ol>
             </div>
-            
-            <Map playgroundss={this.state.playgroundsDetail} searchedPlaygroundsFromList={searchedPlaygrounds}/>
-      </div>
-      
-      
 		)
 	}
 }
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 import List from './List-of-playgrounds'
-import Header from './Header';
+import Header from './Header'
+import Map from './Map'
+import axios from 'axios'
+import ReactPropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
+let searchedPlaygrounds
 
 class MainPage extends Component {
 
@@ -42,27 +48,120 @@ class MainPage extends Component {
         "lng": 17.97441,
         "equipment": []
       }
-    ],    
+    ],
+    places: [],
+    search: ''    
   }
-  
+
+	//Load the map after rendering the DOM
+	componentDidMount = () => {
+		this.loadPlaces()		
+	}
+
+
+	//Load the map with the proper KEY using the initMap function
+	loadScript = () => {
+		createScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDI71ndio2s7kJpHicofZyytGLd0sn3s8E&callback=initMap")
+		window.initMap = this.initMap;
+	}
+
+
+	//Initialize map in a <div id="map"> and set the center and zoom
+	initMap = () => {
+		var map = new window.google.maps.Map(document.getElementById('map'), {
+			center: {lat: 49.936089, lng: 17.973804},
+			zoom: 10			
+		});
+		
+		this.state.places.map(place => {
+			var lat = place.venue.location.lat
+			var lng = place.venue.location.lng
+			var name = place.venue.name
+			
+			var marker = new window.google.maps.Marker({
+				position: {lat: lat, lng: lng},
+				map: map,
+				title: name
+			})
+
+			var infowindow = new window.google.maps.InfoWindow({
+				content: name
+			});
+
+			marker.addListener('click', function() {
+				infowindow.open(map, marker)
+			});
+
+			map.addListener('click', function() {
+				infowindow.close()
+			});
+		})
+	}
+
+
+	//Fetch Foursquare API data using axios (search for "playgrounds" near the "center" of the map)
+	loadPlaces = () => {
+		
+		axios.get('https://api.foursquare.com/v2/venues/explore?client_id=OR4GRJXCDX0LRD5QLD0WNGWYF3X1DRO0DJC02T4LIRY1WCVF&client_secret=5JX4DRFPLNQUZVWUOY2KSUJCEFOX5X3USMZJHHW4PI5RUAEH&v=20180323&limit=50&ll=49.9,17.9&query=playground')
+		.then(response => {
+			// handle success
+			this.setState({ 
+				places: response.data.response.groups[0].items
+			})
+		})
+		.catch(error => {
+			// handle error
+			alert("Sorry! Foursquare data didn't load correctly, not your fault! This error has occured: " + error); 
+		})
+		.then(() => {
+			// load map in any case (with or without Foursquare data)
+			this.loadScript()
+		})
+	}
+
+  static ReactPropTypes = {
+    playgroundsDetail: ReactPropTypes.array.isRequired
+  }
+
+  searchAsk = (ask) => {
+    this.setState({
+      search: ask.trim()
+    })
+  }
+
   render () {
     
+    if (this.state.search) {
+      const result = new RegExp(escapeRegExp(this.state.search), 'i')
+      searchedPlaygrounds = this.state.playgroundsDetail.filter((playground) => result.test(playground.city))
+      searchedPlaygrounds.sort(sortBy('address'))
+    } else {
+      searchedPlaygrounds = this.state.playgroundsDetail
+      searchedPlaygrounds.sort(sortBy('city'))
+    }
+
     return (
       <div className="mainPage">
 				 <Header/>
 			
-				<div id="main">
-          
-					<div id="sideBar">
-						<List playgrounds={this.state.playgroundsDetail}/>            
-					</div>
-		
-					
-		
+				<div id="main">          
+						<List playgrounds={this.state.playgroundsDetail} searchedPlaygroundsFromList={searchedPlaygrounds} search={this.state.search}/>            
+					  <Map playgroundss={this.state.playgroundsDetail} searchedPlaygroundsFromList={searchedPlaygrounds} places={this.state.places}/>				
 				</div>
+        
 			</div>          
 		)
 	}
+}
+
+//Create a <script src="www" async defer></script> tag for inserting the API KEY  
+function createScript(www) {
+	var anchor = window.document.getElementsByTagName("script")[0];
+	var script = window.document.createElement("script");
+	anchor.parentNode.insertBefore(script, anchor);
+	script.src = www;
+	script.async = true;
+	script.defer = true;
 }
 
 export default MainPage
